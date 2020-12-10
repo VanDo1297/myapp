3ca8162881e41e6ec4010016826164c036b841e6
do <ngovando261297@gmail.com>
2020-12-10 10:12:54 +0700

    add-filter

---
+++ src/components/filter/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ShopIcon = React.memo(()=>{
+
+    return (
+        <div className='shop-icon p-relative'>
+            <div className='shop-icon__bg'>aaaaaa</div>
+        </div>
+    )
+})
+
+export default ShopIcon;
